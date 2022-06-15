a6b95ba5a4fbb12a21f80a48aee3d3cc28c819ad
JaehonDev <println@kakao.com>
2022-06-15 14:54:21 +0900

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <ul v-for="mountain in mountains" :key="mountain.id">
+      <NuxtLink :to="`${mountain.continent.toLowerCase()}/${mountain.slug}`">
+        <li>
+          <span>{{ mountain.title }}</span>
+          <span>{{mountain.continent.toLowerCase()}}</span>
+          <span>{{mountain.slug}}</span>
+        </li>
+      </NuxtLink>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData() {
+    const mountains = await fetch(
+      'https://api.nuxtjs.dev/mountains'
+    ).then((res) => res.json())
+
+    return { mountains }
+  }
+}
+</script>
---
+++ pages/mountain.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "mountain"
+}
+</script>
+
+<style scoped>
+
+</style>
